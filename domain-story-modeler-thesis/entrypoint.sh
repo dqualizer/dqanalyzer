6680bf940380965f79f0d05eff39e5a1ed4469f3
Levin <Levin.Kerschberger@novatec-gmbh.de>
2023-05-05 12:31:24 +0200

    added Entrypoint

---
+++ domain-story-modeler-thesis/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the VITE_BACKEND_URL environment variable is set
+if [ -z "$VITE_BACKEND_URL" ]; then
+  echo "Error: 
+       You must specify VITE_BACKEND_URL to a valid URL.
+       For example, "-e VITE_BACKEND_URL=http://localhost" on "docker run"."
+  exit 1
+fi
+
+# Run the env.sh script
+source /usr/share/nginx/html/env.sh
+
+# Start nginx
+nginx -g "daemon off;"
